eecfd0e5ce0bd8e178de2f89e553e2247f4ef48d
Nick Carneiro <burthawk101@gmail.com>
2013-12-14 13:24:28 -0600

    different file looping code

--- postgifs.php
+++ postgifs.php
@@ -3,21 +3,29 @@ require('config.php');
 require($wordpress_path.'/wp-load.php');
 define('UPLOAD_DIR', 'images/');
 $handle = opendir('gifs/');
-while (false !== ($file = readdir($handle))) {
-    echo "creating post for $file\n";
-    continue;
-    date_default_timezone_set('America/Chicago');
-    $title = date('l F jS Y h:i:s A');
+if ($handle = opendir('.')) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            echo "$entry\n";
+            echo "creating post for $file\n";
+            continue;
+            date_default_timezone_set('America/Chicago');
+            $title = date('l F jS Y h:i:s A');
 
-    $content = '<img src="toadbooth/'.$file.'">';
-    $post = array(
-        'post_title' => $title,
-        'post_content' => $content,
-        'post_author' => 1,
-        'post_status' => 'publish'
+            $content = '<img src="toadbooth/'.$file.'">';
+            $post = array(
+                'post_title' => $title,
+                'post_content' => $content,
+                'post_author' => 1,
+                'post_status' => 'publish'
 
-    );
-    $post_id = wp_insert_post($post);
+            );
+            $post_id = wp_insert_post($post);
 
+        }
+    }
+    closedir($handle);
+} else {
+    echo "could not open dir";
 }
 ?>
